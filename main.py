21f8c8a76e31cb7253fc37d5ba76adba359e862c
ssyykim <142778794+ssyykim@users.noreply.github.com>
2024-03-06 12:20:41 +0900

    implemented stack data structure

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("Hello, World!")
+# Stack implementation in python
+
+
+# Creating a stack
+def create_stack():
+    stack = []
+    return stack
+
+
+# Creating an empty stack
+def check_empty(stack):
+    return len(stack) == 0
+# if it returns true, it means that it is empty.
+
+# Adding items into the stack
+def push(stack, item):
+    stack.append(item)
+    print("pushed item: " + item)
+
+
+# Removing an element from the stack
+def pop(stack):
+    if (check_empty(stack)):
+        return "stack is empty"
+
+    return stack.pop()
+
+
+stack = create_stack()
+push(stack, str(1))
+push(stack, str(2))
+push(stack, str(3))
+push(stack, str(4))
+print("popped item: " + pop(stack))
+print("stack after popping an element: " + str(stack))
+
+def peek(stack):
+  if (check_empty(stack)):
+    return "stack is empty"
+  return stack[-1]
+  
